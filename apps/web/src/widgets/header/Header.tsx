d20d3548296b886b1008b6dae1504984ef6cbf42
Maxim Gritsina <maxim.gritsina.working@gmail.com>
2025-08-10 13:01:20 +0300

    feat: remove sidebar - useless

--- apps/web/src/widgets/header/Header.tsx
+++ apps/web/src/widgets/header/Header.tsx
@@ -1,11 +1,7 @@
-import { SidebarTrigger } from '@/shared/ui/kit/Sidebar'
-
 export const Header: React.FC = () => {
   return (
     <header className='flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
-      <div className='flex items-center gap-2 px-4'>
-        <SidebarTrigger className='-ml-1' />
-      </div>
+      <div className='flex items-center gap-2 px-4'>Header </div>
     </header>
   )
 }
